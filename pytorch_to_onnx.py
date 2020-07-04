1281c8cf4ad77f4a19a1dc98d83986acc22b1198
Daniel Persson <mailto.woden@gmail.com>
2020-07-04 23:16:27 +0200

    Updated script.

--- pytorch_to_onnx.py
+++ pytorch_to_onnx.py
@@ -2,31 +2,45 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import onnx
+from collections import OrderedDict
 import tensorflow as tf
 from torch.autograd import Variable
 from onnx_tf.backend import prepare
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+class MLP(nn.Module):
+    def __init__(self, input_dims, n_hiddens, n_class):
+        super(MLP, self).__init__()
+        assert isinstance(input_dims, int), 'Please provide int for input_dims'
+        self.input_dims = input_dims
+        current_dims = input_dims
+        layers = OrderedDict()
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        if isinstance(n_hiddens, int):
+            n_hiddens = [n_hiddens]
+        else:
+            n_hiddens = list(n_hiddens)
+        for i, n_hidden in enumerate(n_hiddens):
+            layers['fc{}'.format(i+1)] = nn.Linear(current_dims, n_hidden)
+            layers['relu{}'.format(i+1)] = nn.ReLU()
+            layers['drop{}'.format(i+1)] = nn.Dropout(0.2)
+            current_dims = n_hidden
+        layers['out'] = nn.Linear(current_dims, n_class)
+
+        self.model= nn.Sequential(layers)
+        print(self.model)
+
+    def forward(self, input):
+        input = input.view(input.size(0), -1)
+        assert input.size(1) == self.input_dims
+        return self.model.forward(input)
+
+
 
 # Load the trained model from file
-trained_model = Net()
-trained_model.load_state_dict(torch.load('models/mnist.pth'))
+trained_dict = torch.load('models/mnist.pth', map_location={'cuda:0': 'cpu'})
+
+trained_model = MLP(784, [256, 256], 10)
+trained_model.load_state_dict(trained_dict)
 
 # Export the trained model to ONNX
 dummy_input = Variable(torch.randn(1, 1, 28, 28)) # one black and white 28 x 28 picture will be the input to the model
@@ -49,4 +63,8 @@ print('tensor_dict:')
 print(tf_rep.tensor_dict)
 
 tf_rep.export_graph('models/mnist.pb')
-tf.saved_model.save(tf_rep, 'models/mnist_saved')
+
+converter = tf.lite.TFLiteConverter.from_frozen_graph(
+        'models/mnist.pb', tf_rep.inputs, tf_rep.outputs)
+tflite_model = converter.convert()
+open("models/mnist.tflite", "wb").write(tflite_model)
